556c703962f3e0275d257ad3db57ce0f4cd07d90
Laura Abitante <laura.abitante@gmail.com>
2017-10-01 22:33:44 -0300

    Created component: tabela-produtos

---
+++ musical-instruments/src/app/tabela-produtos/tabela-produtos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-produtos',
+  templateUrl: './tabela-produtos.component.html',
+  styleUrls: ['./tabela-produtos.component.css']
+})
+export class TabelaProdutosComponent implements OnInit {
+  titulo = "CRUD de Produtos";
+  constructor() { }
+  ngOnInit() { }
+}
